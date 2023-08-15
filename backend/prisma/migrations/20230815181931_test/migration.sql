89244a8069f75478ed328cf6bda7f5cba193afae
Vitor Viviani <potatytoe@gmail.com>
2023-08-15 23:22:30 +0100

    test

---
+++ backend/prisma/migrations/20230815181931_test/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Entry" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "created_at" DATETIME NOT NULL,
+    "scheduled_for" DATETIME NOT NULL
+);
