5d9f23a7c78fbd0c2a912395f3541bb9bc626c9b
Vitor Viviani <potatytoe@gmail.com>
2023-08-15 23:19:48 +0100

    .

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,32 +1,40 @@
 import React from "react";
-import NavBar from './components/NavBar'
-import AllEntries from './routes/AllEntries'
-import NewEntry from './routes/NewEntry'
-import EditEntry from './routes/EditEntry'
-import { EntryProvider } from './utilities/globalContext'
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from "react-router-dom";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import AllEntries from "./routes/AllEntries";
+import EditEntry from "./routes/EditEntry";
+import NewEntry from "./routes/NewEntry";
+import { EntryProvider } from "./utilities/globalContext";
 
 export default function App() {
+  const [darkMode, setDarkMode] = React.useState(false);
+  function change() {
+    setDarkMode(!darkMode);
+    if (darkMode) {
+      document.body.style.backgroundColor = "white";
+    } else {
+      document.body.style.backgroundColor = "#1e293b";
+    }
+  }
+
   return (
-    <section>
-  <Router>
-    <EntryProvider>
-    <NavBar></NavBar>
-      <Routes>
-        <Route path="/" element={<AllEntries/>}>
-        </Route>
-        <Route path="create" element={<NewEntry/>}>
-        </Route>
-        <Route path="edit/:id" element={<EditEntry/>}>
-        </Route>
-      </Routes>
-    </EntryProvider>
-    </Router>
+    <section className={darkMode ? "dark" : ""}>
+      <div className="bg-white dark:bg-slate-800">
+        <button onClick={() => change()} className="text-white bg-blue-400 rounded-md m-2 p-2">
+          {" "}
+          Dark Mode{" "}
+        </button>
+        <Router>
+          <EntryProvider>
+            <NavBar></NavBar>
+            <Routes>
+              <Route path="/" element={<AllEntries />}></Route>
+              <Route path="create" element={<NewEntry />}></Route>
+              <Route path="edit/:id" element={<EditEntry />}></Route>
+            </Routes>
+          </EntryProvider>
+        </Router>
+      </div>
     </section>
-    
   );
 }
